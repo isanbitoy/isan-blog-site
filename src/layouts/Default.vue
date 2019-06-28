c5e024923e9e95b8874503843157b82090a6f46a
isan <isanbitoy@gmail.com>
2019-06-28 15:57:27 +0800

    add folder

---
+++ src/layouts/Default.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="app">
+		<header class="header">
+			<div class="header__left">
+				<Logo v-if="showLogo" />
+			</div>
+
+			<div class="header__right">
+				<ToggleTheme />
+			</div>
+		</header>
+
+		<main class="main">
+			<slot />
+		</main>
+
+		<footer class="footer">
+			<span class="footer__copyright">
+				Copyright © {{ new Date().getFullYear() }}. 
+			</span>
+      		<span class="footer__links">
+      			Powered by <a href="//gridsome.org"> Gridsome </a>
+      		</span>
+		</footer>
+	</div>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+import ToggleTheme from '~/components/ToggleTheme'
+
+export default {
+	props: {
+		showLogo: { default: true }
+	},
+	components: {
+		Logo,
+    	ToggleTheme
+	}
+};
+</script>
