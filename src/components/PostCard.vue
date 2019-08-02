99a010c30854b6bb7ccde113e208381850d2ce0c
isan <isanbitoy@gmail.com>
2019-08-02 21:07:07 +0800

    add files

---
+++ src/components/PostCard.vue
@@ -0,0 +1,32 @@
+<template>
+	<div :class="{'post-card--has-poster' : post.poster}" class="post-card content-box" >
+		<div class="post-card__header">
+			<g-image alt="Cover image" 
+				v-if="post.coverImage" 
+				class="post-card__image" 
+				:src="post.coverImage" />
+		</div>
+		<div class="post-card__content">
+      		<h2 class="post-card__title" v-html="post.title" />
+      		<p class="post-card__description" v-html="post.description" />
+      
+      		<PostMeta class="post-card__meta" :post="post" />
+      		<PostTags class="post-card__tags" :post="post" />
+
+      		<g-link class="post-card__link" :to="post.path">Link</g-link>
+    	</div>
+	</div>
+</template>
+
+<script>
+import PostMeta from '~/components/PostMeta'
+import PostTags from '~/components/PostTags'
+
+export default {
+	components: {
+		PostMeta,
+		PostTags
+	},
+	props: ['post'],
+};
+</script>
